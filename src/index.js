4806b4d76dc66fcade4c1afe14c0af97bfc915c1
Victor Leão <110788743+victorleaoQA@users.noreply.github.com>
2023-08-01 17:40:29 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import App from './components/App';
-import { GameContextProvider } from './store/game-context';
-import './index.css';
+import App from "./components/App";
+import { GameContextProvider } from "./store/game-context";
+import "./index.css";
 
 ReactDOM.render(
-    <GameContextProvider>
-        <App />
-    </GameContextProvider>,
-    document.getElementById('root'));
+  <GameContextProvider>
+    <App />
+  </GameContextProvider>,
+  document.getElementById("root")
+);
